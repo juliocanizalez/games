930487e70d6ae9bcbee463a7c3bf0dbdc61b9322
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-12 15:29:13 -0600

    :hammer: Add routes for components

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <div></div>;
-}
+import RoutePaths from '../definitions/RoutePaths';
+import Home from '../containers/Home';
+import TimeMachine from '../containers/TimeMachine';
+import TicTacToe from '../containers/TicTacToe';
+import NotFound from '../containers/NotFound';
+
+const App: React.FC = () => (
+  <Router>
+    <Switch>
+      <Route exact path={RoutePaths.HOME} component={Home} />
+      <Route exact path={RoutePaths.TIME_MACHINE} component={TimeMachine} />
+      <Route exact path={RoutePaths.TIC_TAC_TOE} component={TicTacToe} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+);
 
 export default App;
