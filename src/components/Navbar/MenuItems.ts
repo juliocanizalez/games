1869705cdcbdf51840ec779b5700055965cf47fd
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-14 17:28:12 -0600

    :hammer: Add menu items for navbar

---
+++ src/components/Navbar/MenuItems.ts
@@ -0,0 +1,17 @@
+export enum Home {
+  TITLE = 'Home',
+  URL = '/home',
+  CLASS_NAME = 'navbar-link',
+}
+
+export enum TimeMachine {
+  TITLE = 'Time Machine',
+  URL = '/machine',
+  CLASS_NAME = 'navbar-link',
+}
+
+export enum TicTacToe {
+  TITLE = 'Tic Tac Toe',
+  URL = '/game',
+  CLASS_NAME = 'navbar-link',
+}
