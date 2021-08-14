3b55d906db8b5e496b272e3a8ec523bfc0f4eafe
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-13 23:32:32 -0600

    :hammer: Add default route and navbar implementations

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 import RoutePaths from '../definitions/RoutePaths';
 import Home from '../containers/Home';
 import TimeMachine from '../containers/TimeMachine';
 import TicTacToe from '../containers/TicTacToe';
 import NotFound from '../containers/NotFound';
+import Navbar from '../components/Navbar/Navbar';
 
 const App: React.FC = () => (
   <Router>
+    <Route exact path='/'>
+      <Redirect to='/home' />
+    </Route>
     <Switch>
+      <Navbar />
       <Route exact path={RoutePaths.HOME} component={Home} />
       <Route exact path={RoutePaths.TIME_MACHINE} component={TimeMachine} />
       <Route exact path={RoutePaths.TIC_TAC_TOE} component={TicTacToe} />
@@ -17,5 +22,4 @@ const App: React.FC = () => (
     </Switch>
   </Router>
 );
-
 export default App;
