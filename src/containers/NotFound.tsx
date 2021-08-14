e77a0c94cbeeb5aaeac4bb864c90ae4f9e5efb6a
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-14 17:26:57 -0600

    :hammer: Add content to not found page

--- src/containers/NotFound.tsx
+++ src/containers/NotFound.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const NotFound: React.FC = () => {
-  return <div></div>;
-};
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
+import '../styles/components/not-found.scss';
+
+const NotFound: React.FC = () => (
+  <div className='not-found'>
+    <Player
+      autoplay
+      loop
+      style={{ width: '400px', height: '400px' }}
+      src='https://assets10.lottiefiles.com/packages/lf20_ghfpce1h.json'
+    >
+      <Controls visible={false} />
+    </Player>
+  </div>
+);
 
 export default NotFound;
