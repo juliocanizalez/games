d7bbed6dc5bdfec984750268129987cf93aa4f66
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-15 14:54:16 -0600

    :hammer: Add time machine custom hook

---
+++ src/hooks/useTimeMachine.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+const useTimeMachine = <T>(value: T): [T | undefined, (position: number) => T | undefined, number, () => void] => {
+  const ref = useRef<T>();
+  const history = useRef<T[]>([]);
+
+  const getPreviousValue = (position: number) => history.current[position];
+
+  const reset = () => {
+    history.current = [];
+  };
+
+  useEffect(() => {
+    ref.current = value;
+    history.current.unshift(value);
+  }, [value]);
+
+  return [ref.current, getPreviousValue, history.current.length, reset];
+};
+
+export default useTimeMachine;
