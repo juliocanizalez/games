b6965c0aaa18db85c4bb17b890d9a79a18e2fbef
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-14 21:40:38 -0600

    :hammer: Add games items

---
+++ src/components/Home/GameItems.ts
@@ -0,0 +1,11 @@
+export enum TimeMachine {
+  NAME = 'Time Machine',
+  IMAGE_STATIC = 'https://res.cloudinary.com/dqaav1s3t/image/upload/v1628991280/Trainee/static_time_j4fkfg.png',
+  IMAGE_ANIMATED = 'https://res.cloudinary.com/dqaav1s3t/image/upload/v1628991278/Trainee/flux-capacitor_d9snt3.gif',
+}
+
+export enum TicTacToe {
+  NAME = 'Tic Tac Toe',
+  IMAGE_STATIC = 'https://res.cloudinary.com/dqaav1s3t/image/upload/v1628991297/Trainee/static_tic_yzage6.png',
+  IMAGE_ANIMATED = 'https://res.cloudinary.com/dqaav1s3t/image/upload/v1628991300/Trainee/kiwi-catscafe_e8x121.gif',
+}
