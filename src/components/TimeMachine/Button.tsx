cabac311dca3ed5d1f0ee17ae1554d08a8ef53ea
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-16 00:02:30 -0600

    :hammer: Add button for time machine

---
+++ src/components/TimeMachine/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface Props {
+  position: number;
+  bgColor: string;
+  isChecked: boolean;
+  isTraveling: boolean | undefined;
+  handleClick(position: number): void;
+}
+
+const Button: React.FC<Props> = ({
+  position,
+  bgColor,
+  isChecked,
+  isTraveling,
+  handleClick,
+}) => (
+  <button
+    type='button'
+    style={{
+      backgroundColor: bgColor,
+      opacity: isChecked ? '100%' : '50%',
+      cursor: isTraveling ? 'not-allowed' : 'pointer',
+      boxShadow: isChecked
+        ? '0 0 4px rgba(0, 0, 0, 0.16), 0 6px 8px rgba(0, 0, 0, 0.26)'
+        : 'none',
+    }}
+    onClick={() => handleClick(position)}
+  >
+    {' '}
+  </button>
+);
+
+export default Button;
