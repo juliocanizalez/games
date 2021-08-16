56efe77df767438d7d326b5e62c1707de7bcb4f3
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-16 00:01:37 -0600

    :hammer: Add Time machine reducer

---
+++ src/reducers/TimeMachine/TimeMachineReducer.ts
@@ -0,0 +1,27 @@
+import TimeMachineActionsTypes from './TimeMachineActionTypes';
+import InitialState from './InitialState';
+import { ITimeMachineReducerAction } from './ITimeMachine';
+
+const TimeMachineReducer = (state = InitialState, action: ITimeMachineReducerAction) => {
+  const { currentPosition, isTraveling, squares } = action.payload;
+
+  switch (action.type) {
+    case TimeMachineActionsTypes.SET_NEXT_OPTION:
+      return {
+        ...state,
+        squares,
+        historySquares: squares,
+      };
+    case TimeMachineActionsTypes.MOVE_IN_TIME:
+      return {
+        ...state,
+        currentPosition,
+        isTraveling,
+        squares,
+      };
+    default:
+      return state;
+  }
+};
+
+export default TimeMachineReducer;
