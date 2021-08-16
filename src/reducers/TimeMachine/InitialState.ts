73470bff5c498b4e5e99661d404c57d7e248423f
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-15 23:59:06 -0600

    :hammer: Update Time machine initial state

--- src/reducers/TimeMachine/InitialState.ts
+++ src/reducers/TimeMachine/InitialState.ts
@@ -2,11 +2,11 @@ import { ITimeMachineReducerState } from './ITimeMachine';
 
 const size = 4;
 const initGameValues = Array(size * size).fill(false) as boolean[];
-const timeMachineInitialState = {
+const InitialState = {
   currentPosition: 0,
   isTraveling: false,
   squares: initGameValues,
   historySquares: initGameValues,
 } as ITimeMachineReducerState;
 
-export default timeMachineInitialState;
+export default InitialState;
