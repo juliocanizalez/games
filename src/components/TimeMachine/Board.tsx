2db7fedfa4879ca7d095c73a27df22c9f8d734df
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-16 00:44:51 -0600

    :hammer: Add board component from time machine

---
+++ src/components/TimeMachine/Board.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import Button from './Button';
+
+interface Props {
+  squares: boolean[] | undefined;
+  isTraveling: boolean | undefined;
+  handleClick(position: number): void;
+}
+const Board: React.FC<Props> = ({ squares, isTraveling, handleClick }) => {
+  const [colors] = useState<string[]>([
+    '#264653',
+    '#2a9d8f',
+    '#e9c46a',
+    '#e76f51',
+    '#a8dadc',
+    '#e63946',
+    '#457b9d',
+    '#8d99ae',
+    '#264653',
+    '#2a9d8f',
+    '#e9c46a',
+    '#e76f51',
+    '#a8dadc',
+    '#e63946',
+    '#457b9d',
+    '#8d99ae',
+  ]);
+
+  return (
+    <div>
+      {squares &&
+        squares.map((isChecked: boolean, position: number) => (
+          <Button
+            key={colors[position]}
+            position={position}
+            bgColor={colors[position]}
+            isChecked={isChecked}
+            handleClick={handleClick}
+            isTraveling={isTraveling}
+          />
+        ))}
+    </div>
+  );
+};
+
+export default Board;
