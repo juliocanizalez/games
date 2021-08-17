93fed0397e6e4cc5c75c8bfdf46def87fd153aa2
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-16 20:13:54 -0600

    :hammer: Add more props for button as a common component

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -4,16 +4,24 @@ import '../../styles/components/TimeMachine/button.scss';
 
 interface Props {
   text: string;
+  className: string;
   disabled: boolean;
   getPreviousValue?: (value: number) => void;
   increment?: boolean;
   handleResume?: () => void;
 }
-const Button: React.FC<Props> = ({ text, disabled, increment, getPreviousValue, handleResume }) => {
+const Button: React.FC<Props> = ({
+  text,
+  className,
+  disabled,
+  increment,
+  getPreviousValue,
+  handleResume,
+}) => {
   return (
     <button
       type='button'
-      className='option'
+      className={className}
       disabled={disabled}
       onClick={() => {
         if (increment && getPreviousValue) {
