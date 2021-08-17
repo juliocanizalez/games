7bf2d8a9bc8023bf94adebe29835dd048f9532cf
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-16 19:04:56 -0600

    :hammer: Update color codes

--- src/components/TimeMachine/Board.tsx
+++ src/components/TimeMachine/Board.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+
 import Button from './BoardButton';
 
 interface Props {
@@ -16,18 +17,18 @@ const Board: React.FC<Props> = ({ squares, isTraveling, handleClick }) => {
     '#e63946',
     '#457b9d',
     '#8d99ae',
-    '#264653',
-    '#2a9d8f',
-    '#e9c46a',
-    '#e76f51',
-    '#a8dadc',
-    '#e63946',
-    '#457b9d',
-    '#8d99ae',
+    '#264654',
+    '#2a9d8e',
+    '#e9c46b',
+    '#e76f52',
+    '#a8dade',
+    '#e63947',
+    '#457b9e',
+    '#8d99af',
   ]);
 
   return (
-    <div>
+    <div className='board'>
       {squares &&
         squares.map((isChecked: boolean, position: number) => (
           <Button
