ed22504a5739f3b6c2c39811d64e2ace05ed4433
Julio Canizalez <juliocanizalez@outlook.com>
2021-08-16 19:16:47 -0600

    :hammer: Add action types for tic tac toe

---
+++ src/reducers/TicTacToe/TicTacToeActionTypes.ts
@@ -0,0 +1,10 @@
+enum TicTacToeActionTypes {
+  SET_ERROR = 'SET_ERROR',
+  SET_NEXT_PLAY = 'SET_NEXT_PLAY',
+  SET_WINNER = 'SET_WINNER',
+  MOVE_IN_TIME = 'MOVE_IN_TIME',
+  RESET = 'RESET',
+  REPLAY = 'REPLAY',
+}
+
+export default TicTacToeActionTypes;
